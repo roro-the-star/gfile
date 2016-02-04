8f3bc8281399a0de4d07f7bbe0e692c9995db572
riehli_r <riehli_r@etna-alternance.com>
2016-02-04 23:42:14 +0100

    Add gfile.rb

---
+++ gfile.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+if ARGV.count < 1
+	puts "Usage: ruby ./test.rb file1 [file2[...]]\n"
+else
+	ARGV.each do |arg|
+		puts "#{arg}:"
+		if /.c$/.match(arg)
+			file = File.new(arg	, "w")
+			file.puts("/*File:#{arg}                   */")
+			file.puts("/*Author: Romain RIEHLING   */")
+			file.puts("/*Email:riehli_r@etna-alternance.net*/")
+			file.puts("/*Creation date: #{Time.now}*/")
+			file.puts
+			file.close
+			puts "File created\n"
+		else
+			puts "File type not recognized"
+		end
+	end
+end
