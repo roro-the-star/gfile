7e8a36cab950b0e233982473bbd710b8098836ca
riehli-r <riehli_r@etna-alternance.net>
2016-02-05 23:33:44 +0100

    Function

--- gfile.rb
+++ gfile.rb
@@ -1,27 +1,45 @@
 #!/usr/bin/env ruby
 
-if ARGV.count < 1
-	puts "Usage: ruby ./test.rb file1 [file2[...]]\n"
-else
-	file_conf = File.new(".gfile", "r")
+def get_info()
+	if File.exists?("./.gfile")
+		file_conf = File.new(".gfile", "r")
+	elsif File.exists?("~/.gfile")
+		file_conf = File.new("~/.gfile", "r")
+	else
+		puts "There isn't any conf file"
+		return false		
+	end
 	tab = {"key"=>"value"}
 	while (line = file_conf.gets)
 		result = /^([a-z]+):([a-z_@\-\. ]+)$/.match(line)	
 		tab[result[1]] = result[2]
 	end
+	return tab
+end
+
+def check_cmd()
+	if ARGV.count < 1
+		puts "Usage: ruby ./test.rb file1 [file2[...]]\n"
+		return false
+	else
+		return true
+	end
+end
+
+if check_cmd()	
 	ARGV.each do |arg|
 		puts "#{arg}:"
 	  file = File.new(arg	, "w")
-		if /.c$/.match(arg)#Check if the file has a recognized type
+		tab = get_info()
+		if /.c$/.match(arg) && tab != false #Check if the file has a recognized type
+			file.puts ("/*========================================")
 			tab.each do |key, value|
-				file.puts("/*#{key}: #{value}*/")
+				if key != "key"
+					file.puts("#{key}: #{value}")
+				end
 			end
-			
-			##file.puts("/*File:#{arg}            */")
-			##file.puts("/*Author: Name  */")
-			##file.puts("/*Email:email@example.net*/")#Will be replace by tab info
-			file.puts("/*Creation date: #{Time.now}*/")
-			##file.puts
+			file.puts("Creation date: #{Time.now}")
+			file.puts("=========================================*/")
 			file.close
 			puts "File created\n"
 		else
