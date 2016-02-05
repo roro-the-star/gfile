36bb8d11f053e1b54cac0a0726e3119d13360b2b
riehli-r <riehli_r@etna-alternance.net>
2016-02-05 01:05:29 +0100

    Implementation of reading .gfile |NOT FINISH|

--- gfile.rb
+++ gfile.rb
@@ -3,13 +3,19 @@
 if ARGV.count < 1
 	puts "Usage: ruby ./test.rb file1 [file2[...]]\n"
 else
+	file_conf = File.new(".gfile", "r")
+	tab = {"key"=>"value"}
+	while (line = file_conf.gets)
+		result = /^([a-z]+):([a-z]+)$/.match(line)	
+		#TODO: make each tab's case for each line
+	end
 	ARGV.each do |arg|
 		puts "#{arg}:"
-		if /.c$/.match(arg)
+		if /.c$/.match(arg)#Check if the file has a recognized type
 			file = File.new(arg	, "w")
 			file.puts("/*File:#{arg}            */")
-			file.puts("/*Author: Romain  */")
-			file.puts("/*Email:riehli_r@etna-alternance.net*/")
+			file.puts("/*Author: Name  */")
+			file.puts("/*Email:email@example.net*/")#Will be replace by tab info
 			file.puts("/*Creation date: #{Time.now}*/")
 			file.puts
 			file.close
