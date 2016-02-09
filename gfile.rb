13fc32476ff8496c5e7d26464eaf7cd61ada69bb
riehli-r <riehli_r@etna-alternance.net>
2016-02-09 12:07:26 +0100

    check if file already exist

--- gfile.rb
+++ gfile.rb
@@ -7,29 +7,48 @@ require "./get_type.rb"
 require "./init.rb"
 require "./last_line.rb"
 
+def file_exist(path)
+  if File.exists?(path)
+		puts "File already exist.\nOverwrite ? [y/n]"
+		choice = 'e'
+		while choice != 'y' || choice != 'n' 
+			choice = $stdin.gets.chomp
+			if choice == 'y'
+				return true
+			elsif choice == 'n'
+				return false
+			end
+		end	
+	else
+			return true		
+	end
+end
+
 if check_cmd()	
 	ARGV.each do |arg|
 		puts "#{arg}:"
-	  file = File.new(arg	, "w")
-		tab = get_info()
-		com_type = get_type(arg)
-		if com_type && tab != false 
-			file.puts ("#{com_type[0]}========================================")
-			tab.each do |key, value|
-				if key != "key"
- 					if com_type[0] == com_type[1]
-						file.puts("#{com_type[1]}#{key}: #{value}")
-					else
-						file.puts("#{key}: #{value}")
+		if file_exist(arg)
+	  	file = File.new(arg	, "w")
+			tab = get_info()
+			com_type = get_type(arg)
+			if com_type && tab != false 
+				file.puts ("#{com_type[0]}========================================")
+				tab.each do |key, value|
+					if key != "key"
+ 						if com_type[0] == com_type[1]
+							file.puts("#{com_type[1]}#{key}: #{value}")
+						else
+							file.puts("#{key}: #{value}")
+						end
 					end
 				end
+				creation_date(file, com_type)
+				last_line(file, com_type)
+				file.close
+				puts "File created"
+			else
+				puts "File type not recognized"
 			end
-			creation_date(file, com_type)
-			last_line(file, com_type)
-			file.close
-			puts "File created"
-		else
-			puts "File type not recognized"
 		end
 	end
 end
