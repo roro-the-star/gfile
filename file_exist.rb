6e8bf13a56e53aa6fba0038751193dc0712a2237
riehli-r <riehli_r@etna-alternance.net>
2016-02-09 15:15:13 +0100

    File_exist

---
+++ file_exist.rb
@@ -0,0 +1,16 @@
+def file_exist(path)
+  if File.exists?(path)
+    puts "File already exist.\nOverwrite ? [y/n]"
+    choice = 'e'
+    while choice != 'y' || choice != 'n'
+      choice = $stdin.gets.chomp
+      if choice == 'y'
+        return true
+      elsif choice == 'n'
+        return false
+      end
+    end
+  else
+    return true
+  end
+end
