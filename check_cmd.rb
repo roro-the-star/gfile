70855d4902f32735ba6860122ac76c3b6a96a59f
riehli-r <riehli_r@etna-alternance.net>
2016-02-10 00:36:51 +0100

    Check arg count

--- check_cmd.rb
+++ check_cmd.rb
@@ -1,10 +1,20 @@
+def usage()
+  puts "Usage: gfile.rb file1 [file2[...]]\n"
+  puts "Or you can init:"
+  puts "gfile.rb init name"
+end
+
 ##Check if arguments are not wrong
 def check_cmd()
   if ARGV.count < 1
-    puts "Usage: ruby ./test.rb file1 [file2[...]]\n"
+    usage()
     return false
   elsif ARGV[0] == "init"
-    init(ARGV[1])
+    if ARGV.count < 2
+      usage()
+    else
+      init(ARGV[1])
+    end
     return false
   else
     return true
