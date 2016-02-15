31d957fa361ae1f9298a567b8048c4035550123e
riehli-r <riehli_r@etna-alternance.net>
2016-02-15 22:51:22 +0100

    file in directory rails

--- rails/gfile/app/controllers/pages_controller.rb
+++ rails/gfile/app/controllers/pages_controller.rb
@@ -5,7 +5,9 @@ include Mongo
 class PagesController < ApplicationController
   def home
     client = Mongo::Client.new(['127.0.0.1:27017'], :database => "gfile")
-    coll = client[:directory]
-    @coll_directory = coll.find()
-  end
+    coll_directory = client[:directory]
+    coll_file = client[:file]
+    @coll_directory = coll_directory.find()
+    @coll_file = coll_file.find() 
+ end
 end
